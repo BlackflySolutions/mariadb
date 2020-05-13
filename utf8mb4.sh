413c4cda09b196e44b81a456bd60f4afb53ced65
Alan Dixon <alan.g.dixon@gmail.com>
2020-05-13 10:15:08 -0400

    Use a _ci collation for case insensitivity

--- utf8mb4.sh
+++ utf8mb4.sh
@@ -2,7 +2,7 @@
 
 # utf8mb4.sh [<charset> <collation>]
 # changes MySQL/MariaDB charset and collation for civicrm database
-# all tables prefixed with civicrm and all columns in those tables
+# all tables and all columns in those tables
 
 DB="$CIVICRM_DATABASE"
 CHARSET="$3"
@@ -11,13 +11,13 @@ COLL="$4"
 [ -n "$DB" ] || exit 1
 [ -n "$MYSQL_PASSWORD" ] || exit 1
 [ -n "$CHARSET" ] || CHARSET="utf8mb4"
-[ -n "$COLL" ] || COLL="utf8mb4_bin"
+[ -n "$COLL" ] || COLL="utf8mb4_general_ci"
 
 PW="--password=""$MYSQL_PASSWORD"
 
 echo $DB
 echo "ALTER DATABASE $DB CHARACTER SET $CHARSET COLLATE $COLL;" | mysql -u root -h vsql "$PW"
-echo "USE $DB; SHOW TABLES LIKE 'civicrm_%';" | mysql -u root -h vsql -s "$PW" | (
+echo "USE $DB; SHOW TABLES;" | mysql -u root -h vsql -s "$PW" | (
     while read TABLE; do
         echo $DB.$TABLE
         echo "ALTER TABLE $TABLE CONVERT TO CHARACTER SET $CHARSET COLLATE $COLL;" | mysql -u root -h vsql "$PW" $DB
